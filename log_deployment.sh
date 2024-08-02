1ec71d6c7a353acb9c44665dafad7bf843026373
Tanmay Srivastava <tanmayshrivastava53@gmail.com>
2024-08-02 11:03:39 +0100

    Add deployment logging script

---
+++ log_deployment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+DEPLOYMENT_LOG="deployment_log.txt"
+COMMIT_HASH=$(git rev-parse HEAD)
+VERSION="1.0.0" # Replace with your method to get the version, if applicable
+
+# Capture the current date and time
+DEPLOYMENT_DATE=$(date '+%Y-%m-%d')
+DEPLOYMENT_TIME=$(date '+%H:%M:%S')
+
+# Log the deployment details
+echo "$DEPLOYMENT_DATE | $DEPLOYMENT_TIME | Version: $VERSION | Commit Hash: $COMMIT_HASH" >> $DEPLOYMENT_LOG
