1830da08628d0b0e1f7dccd9b3900f3e03b963ef
Tanmay Srivastava <tanmayshrivastava53@gmail.com>
2024-08-02 18:07:11 +0100

    Added Cart Controller for e-commerce application

--- log_deployment.sh
+++ log_deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DEPLOYMENT_LOG="deployment_log.txt"
 COMMIT_HASH=$(git rev-parse HEAD)
-VERSION="1.0.0" # Replace with your method to get the version, if applicable
+VERSION="1.0.1" # Replace with your method to get the version, if applicable
 
 # Capture the current date and time
 DEPLOYMENT_DATE=$(date '+%Y-%m-%d')
